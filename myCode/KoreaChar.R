e5eeefaa0ac1a47edf4c6310c9f5b93cb9de0a81
Junsok Huhh <anarinsk@gmail.com>
2016-07-21 13:27:03 +0900

    일단, 여기서 접는다. 일단, 지도가 최신도 아니고 인코딩 문제가...

--- myCode/KoreaChar.R
+++ myCode/KoreaChar.R
@@ -1,24 +1,54 @@
-install.packages("devtools")
-devtools::install_github("cardiomoon/Kormaps", force=T)
-install.packages('tmap')
-install.packages("magrittr")
+#install.packages("devtools")
+#devtools::install_github("cardiomoon/Kormaps", force=T)
+#install.packages('tmap')
+#install.packages("magrittr")
 
 library(Kormaps)
 library(tmap)
 library(magrittr)
 library(leaflet)
 library(extrafont)
+library(dplyr)
 
+### [X] Override locale -> change col and change col name : Colnames are not changed
+### [ ] Override locale -> change col name and change col : Colnames are not changed
+
+
+### Locale adjustment
+### [ ] Is this necessary?
 Sys.setlocale("LC_ALL") # See what locale used in your OS and system
 Sys.setlocale("LC_ALL", "C") # Override locale
 Sys.getlocale("LC_ALL") # Check
 
-tlist <- colnames(korpopmap2@data)
-tlist
+### Column name transform
+### [ ]
+
+### locale 조정 필수
+
+
+fuckwindows <- function(df, colnum)
+{
+    fres <- sapply(df[colnum], function(x){iconv(x, "UTF-8", "CP949")})
+    df[colnum] <- fres
+    return(df)
+}
+
+test <- korpopmap1@data
+
 
 # For Windows change character encoding from UTF-8 to CP949
-colnames(korpopmap1@data) <- iconv(colnames(korpopmap1@data), "UTF-8", "CP949")
-colnames(korpopmap1@data)[21]
+colnames(test) <- iconv(colnames(test), "UTF-8", "CP949")
+kornames <- colnames(test)
+
+fuckset <- which(sapply(test, is.character))
+
+for(ii in fuckset)
+{
+    test <- fuckwindows(test, ii)
+}
+
+test
+
 
 colnames(korpopmap2@data) <- iconv(colnames(korpopmap2@data), "UTF-8", "CP949")
 colnames(korpopmap2@data)[21]
@@ -33,12 +63,16 @@ areacode
 korpopmap2@data[15] <- iconv(korpopmap2@data[15], "UTF-8", "CP949")
 korpopmap2@data[15]
 
+
+str(areacode)
 areacode$name <- iconv(areacode$name, "UTF-8", "CP949")
 areacode$name2 <- iconv(areacode$name2, "UTF-8", "CP949")
 
-str(areacode)
 
-myarea2code("서울")
+### Check Areacode
+
+
+
 
 #' @param x a numeric vector
 is.integer0 <- function(x) { is.integer(x) && length(x) == 0L}
